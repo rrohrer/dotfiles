248f347af89b4430d50bca85c802181e2e6235de
rrohrer <ryan.rohrer@gmail.com>
2016-08-06 18:20:16 -0700

    add a bash_profile that sets the path up for golang

--- platform/osx/install.sh
+++ platform/osx/install.sh
@@ -2,4 +2,5 @@ brew install cmake cscope ctags macvim the_silver_searcher wget
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.ctags ~/.ctags
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 vim +PlugInstall +qall
