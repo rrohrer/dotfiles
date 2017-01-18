7064c9b71bd5acc7910601014d83afd8c61be963
Ryan Rohrer <ryrohr@microsoft.com>
2017-01-18 08:18:13 -0800

    started windows install script

---
+++ platform/windows/install.bat
@@ -0,0 +1,4 @@
+mklink /H %USERPROFILE%\.vimrc %USERPROFILE%\dotfiles\.vimrc
+mklink %USERPROFILE%\vimfiles %USERPROFILE%\dotfiles\.vim
+mklink %USERPROFILE%\.vim %USERPROFILE%\dotfiles\.vim
+mklink /H %USERPROFILE%\.ctags %USERPROFILE%\dotfiles\.ctags
